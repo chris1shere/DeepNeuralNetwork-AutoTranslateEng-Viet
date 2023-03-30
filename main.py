26ae4a2db8af23f3fc0e1a0060834d6afe45e3da
chris1shere <pokuonglao123@gmail.com>
2023-03-30 09:21:41 -0700

    3/30 change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the data into a pandas DataFrame
-data = pd.read_csv('C:/Users/chris/Desktop/PYTHON/English-Viet Translation/data.csv', header=None, names=['english', 'vietnamese'])
+data = pd.read_csv('C:\Users\chris\Desktop\PYTHON\English-Viet Translation\data.csv', header=None, names=['english', 'vietnamese'])
 
 # Print the first few rows of the data
 print(data.head())
